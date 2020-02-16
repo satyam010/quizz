b513767a66d860ab456f01df127d9663d08a567d
Satyam Prakash <prakashsatyam6@gmail.com>
2020-02-17 00:27:43 +0530

    Updates

---
+++ precache-manifest.255517dc9ed8a82323c35af7b07744b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a1f612c90cb4c1acb04a2746493c66",
+    "url": "/quizz/index.html"
+  },
+  {
+    "revision": "74658181c4a05a3375f1",
+    "url": "/quizz/static/css/main.5d3da506.chunk.css"
+  },
+  {
+    "revision": "732ea4ea642f9835c9cc",
+    "url": "/quizz/static/js/2.68ec26d8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/quizz/static/js/2.68ec26d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74658181c4a05a3375f1",
+    "url": "/quizz/static/js/main.525ea45c.chunk.js"
+  },
+  {
+    "revision": "824d1bec5d977fe83555",
+    "url": "/quizz/static/js/runtime-main.e978970e.js"
+  }
+]);
